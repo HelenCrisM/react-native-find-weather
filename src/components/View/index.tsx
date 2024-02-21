723023122c3d2286ed09562ca5392aae46f996ac
Helen Cris <helencristavares@gmail.com>
2024-02-20 21:15:21 -0300

    ✨ feat: created view component

---
+++ src/components/View/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledView } from './styles';
+
+const View = ({ children }: any): JSX.Element => {
+	return <StyledView>{children}</StyledView>;
+};
+
+export default View;
+
+
---
+++ src/components/View/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import { colors } from '../../theme/colors';
+
+export const StyledView = styled.SafeAreaView`
+	flex: 1;
+	justify-content: center;
+  align-items: center;
+  padding: 11%;
+	background-color: ${colors.dark300};
+`;
