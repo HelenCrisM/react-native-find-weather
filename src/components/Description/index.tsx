faea0586dbbd2483d5400f7401cb57bc608adcc3
Helen Cris <helencristavares@gmail.com>
2024-02-25 13:37:43 -0300

    🦄 refactor: delete Description component

--- src/components/Description/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Styled from './styles';
-
-const Description = ({ children }: any): JSX.Element => {
-	return <Styled.Description>{children}</Styled.Description>;
-};
-
-export default Description;
