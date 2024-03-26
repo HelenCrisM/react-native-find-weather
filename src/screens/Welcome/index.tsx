860f4b296d798659e1e97c4ce9d4e07d2e58a561
Helen Cris <helencristavares@gmail.com>
2024-03-26 20:58:42 -0300

    🦄 refactor: refactor welcome page

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,41 +1,101 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Button from '../../components/Button';
-import Title from '../../components/Title';
-import View from '../../components/View';
-import { Image } from 'react-native';
 import Text from '../../components/Text';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { IStackRoutes } from '../../@types/stack.routes';
 import theme from '../../theme';
 
-function Welcome() {
-	const navigation = useNavigation<StackNavigationProp<IStackRoutes>>();
+import CloudAndThunderPNG from '../../assets/cloud-and-thunder.png';
+
+import Styled from './styles';
+
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { USER_FIRST_TIME } from '../../storage/storage.config';
+import { CommonActions } from '@react-navigation/native';
+import { IStackRoutes } from '../../routes/stack.type.routes';
+import Divider from '../../components/Divider';
+
+type WelcomeScreenNavigationProp = NativeStackNavigationProp<IStackRoutes, 'Welcome'>;
+
+type Props = {
+	navigation: WelcomeScreenNavigationProp;
+};
+
+const LetterBold = () => (
+	<Text fontFamily={theme.fontFamily.OverpassBold} fontSize={theme.fontSize.md22} color={theme.colors.gray100}>
+		Weather
+	</Text>
+);
+
+const Welcome = ({ navigation }: Props): JSX.Element => {
+	const handleGoneThroughWelcome = async () => {
+		await AsyncStorage.setItem(USER_FIRST_TIME, 'no').catch((error) => {
+			console.log('Error inserting USER_FIRST_TIME into storage: ', error);
+		});
+	};
+
+	const handleNavigation = useCallback(() => {
+		navigation.dispatch(
+			CommonActions.reset({
+				index: 0,
+				routes: [
+					{
+						name: 'TabRoutes',
+					},
+				],
+			})
+		);
+	}, []);
 
 	return (
-		<View>
-			<Image source={require('../../assets/cloud-and-thunder.png')} />
-			<Title>Descubra o Clima na sua Cidade</Title>
-			<Text
-				fontFamily={theme.fontFamily.OverpassRegular}
-				fontSize={theme.fontSize.md22}
-				color={theme.colors.gray100}
-				textAlign="center"
-			>
-				Com o FindWeather nunca ficou tão fácil ter a previsão do tempo na palma da sua mão{' '}
-			</Text>
-			<Button onPress={() => navigation.navigate('Home')}>
+		<Styled.Container>
+			<Styled.ContainerImage>
+				<Styled.Image source={CloudAndThunderPNG} />
+			</Styled.ContainerImage>
+
+			<Divider top={34} />
+
+			<Styled.ContainerTextButton>
+				<Text
+					fontFamily={theme.fontFamily.OverpassSemiBold}
+					fontSize={theme.fontSize.xxl33}
+					color={theme.colors.white}
+					style={{ width: 300, textAlign: 'center', alignSelf: 'center' }}
+				>
+					Descubra o Clima na sua Cidade
+				</Text>
+				<Divider top={24} />
 				<Text
 					fontFamily={theme.fontFamily.OverpassRegular}
 					fontSize={theme.fontSize.md22}
-					color={theme.colors.white}
-					textAlign="center"
+					color={theme.colors.gray100}
+					style={{ textAlign: 'center' }}
 				>
-					Iniciar
+					Com o Find
+					<LetterBold /> nunca ficou tão fácil ter a previsão do tempo na palma da sua mão
 				</Text>
-			</Button>
-		</View>
+
+				<Divider top={70} />
+
+				<Button
+					backgroundColor={theme.colors.dark300}
+					borderColor={theme.colors.gray300}
+					onPress={() => {
+						handleNavigation();
+						handleGoneThroughWelcome();
+					}}
+				>
+					<Text
+						fontFamily={theme.fontFamily.OverpassRegular}
+						fontSize={theme.fontSize.md22}
+						color={theme.colors.white}
+						textAlign='center'
+					>
+						Iniciar
+					</Text>
+				</Button>
+			</Styled.ContainerTextButton>
+		</Styled.Container>
 	);
-}
+};
 
 export default Welcome;
