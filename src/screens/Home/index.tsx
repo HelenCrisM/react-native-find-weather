28439c7018925a9ae8dfbb8570918d852144aa2a
Helen Cris <helencristavares@gmail.com>
2024-03-26 20:57:13 -0300

    🦄 refactor: refactor code

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,414 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Platform,
+  SectionList,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import Styled from './styles';
+import Text from "../../components/Text";
+import Divider from "../../components/Divider";
+import WeatherDescription from "../../components/WeatherDescription";
+import CardHourTemperature from "../../components/CardHourTemperature";
 
-import EmptyStateContent from '../../utils/EmptyStateContent';
+import theme from "../../theme";
+import Styled from "./styles";
 
-const Home = (): JSX.Element => {
-	return (
-		<Styled.ScrollView>
-			<EmptyStateContent />
-		</Styled.ScrollView>
-	);
+import { Ionicons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+import DropMiniaturePNG from "../../assets/drop-miniature.png";
+import WindMiniaturePNG from "../../assets/wind-miniature.png";
+import RainingCloudMiniaturePNG from "../../assets/raining-cloud-miniature.png";
+
+import ClimateChangePNG from "../../assets/climate-change.png";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { IStackRoutes } from "../../routes/stack.routes";
+import { useFocusEffect } from "@react-navigation/native";
+import {
+  ICurrent,
+  ILocation,
+  ISearchData,
+  IForecastData,
+} from "../../utils/search.interface";
+import {
+  IForecast5Days,
+  IForecastDay,
+} from "../../utils/forecast5days.interface";
+import { CITY_NAME, COUNTRY_CODE } from "../../storage/storage.config";
+import { FindWeatherAPI } from "../../services/findweather-api";
+import { formatDate } from "../../utils/formatDate";
+import { FindWeatherOpenWeatherAPI } from "../../services/findweather-api-openweather";
+import { forecastConditionsIcons } from "../../utils/forecastIcon";
+
+export type HomeScreenNavigationProp = NativeStackNavigationProp<
+  IStackRoutes,
+  "Home"
+>;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+
+interface IFullContentData {
+  location: ILocation;
+  current: ICurrent;
+  forecast: {
+    forecastday: Array<IForecastData>;
+  };
+  date: string;
+  navigation: HomeScreenNavigationProp;
+  forecast5Days: Array<IForecastDay>;
+}
+
+const EmptyStateContent = ({ navigation }: Props) => {
+  return (
+    <Styled.ContainerEmptyState>
+      <Divider top={30} />
+
+      <Text
+        fontFamily={theme.fontFamily.OverpassRegular}
+        fontSize={theme.fontSize.xxl33}
+        color={theme.colors.white}
+      >
+        Find
+        <Text
+          fontFamily={theme.fontFamily.OverpassBold}
+          fontSize={theme.fontSize.xxl33}
+          color={theme.colors.white}
+        >
+          Weather
+        </Text>
+      </Text>
+
+      <Image source={ClimateChangePNG} />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Search")}
+        activeOpacity={0.75}
+      >
+        <Text
+          fontFamily={theme.fontFamily.OverpassRegular}
+          fontSize={theme.fontSize.md22}
+          color={theme.colors.gray100}
+          style={{ textDecorationLine: "underline", textAlign: "center" }}
+        >
+          Selecione aqui um local e {"\n"} encontre o clima em tempo real
+        </Text>
+      </TouchableOpacity>
+
+      <Divider top={30} />
+    </Styled.ContainerEmptyState>
+  );
+};
+
+const FullContent = ({
+  location,
+  current,
+  forecast,
+  forecast5Days,
+  date,
+  navigation,
+}: IFullContentData) => {
+  const iOS = Platform.OS === "ios";
+  const { humidity, wind_kph } = current;
+  const { daily_will_it_rain } = forecast.forecastday[0].day;
+
+  const dataWeatherDescription = [
+    {
+      id: 1,
+      icon: DropMiniaturePNG,
+      value: `${humidity}%`,
+      text: "Umidade",
+    },
+
+    {
+      id: 2,
+      icon: WindMiniaturePNG,
+      value: `${Math.floor(wind_kph)}km/h`,
+      text: "Veloc. Vento",
+    },
+
+    {
+      id: 3,
+      icon: RainingCloudMiniaturePNG,
+      value: `${Math.floor(daily_will_it_rain)}%`,
+      text: "Chuva",
+    },
+  ];
+
+  return (
+    <>
+      <Styled.Container>
+        <Divider top={27} />
+
+        <Styled.LocationIconContainer>
+          <Ionicons
+            name="location-sharp"
+            size={22}
+            color={theme.colors.white}
+          />
+
+          <Styled.LocationTextContainer>
+            <Styled.LocationCityCountryContainer>
+              <Text
+                fontFamily={theme.fontFamily.OverpassRegular}
+                fontSize={theme.fontSize.sm18}
+                color={theme.colors.white}
+              >
+                {""} {location.name}, {""}
+              </Text>
+
+              <Text
+                fontFamily={theme.fontFamily.OverpassRegular}
+                fontSize={theme.fontSize.sm18}
+                color={theme.colors.white}
+              >
+                {location.country}
+              </Text>
+            </Styled.LocationCityCountryContainer>
+
+            <Divider top={3} />
+
+            <Text
+              fontFamily={theme.fontFamily.OverpassRegular}
+              fontSize={theme.fontSize.xs16}
+              color={theme.colors.gray100}
+            >
+              {""} {date}
+            </Text>
+          </Styled.LocationTextContainer>
+        </Styled.LocationIconContainer>
+
+        <Divider top={19} />
+
+        <Styled.ImageContainer>
+          <Styled.Image
+            source={forecastConditionsIcons(current.condition.text)}
+          />
+        </Styled.ImageContainer>
+
+        <Divider top={iOS ? 10 : -10} />
+
+        <Styled.ContainerTemperature>
+          <Text
+            fontFamily={theme.fontFamily.OverpassBold}
+            fontSize={theme.fontSize.giant76}
+            color={theme.colors.white}
+          >
+            {Math.round(current.temp_c)}
+          </Text>
+          <Text
+            fontFamily={theme.fontFamily.OverpassBold}
+            fontSize={theme.fontSize.lg30}
+            color={theme.colors.white}
+            style={{
+              alignSelf: "center",
+              height: 80,
+            }}
+          >
+            º
+          </Text>
+        </Styled.ContainerTemperature>
+
+        <Divider top={iOS ? 0 : -10} />
+
+        <Text
+          fontFamily={theme.fontFamily.OverpassRegular}
+          fontSize={theme.fontSize.md22}
+          color={theme.colors.gray100}
+          style={{ textAlign: "center" }}
+        >
+          {current.condition.text}
+        </Text>
+      </Styled.Container>
+
+      <Divider top={30} />
+
+      <WeatherDescription data={dataWeatherDescription} />
+
+      <Divider top={30} />
+
+      <Styled.TodayAnd5NextDaysContainer>
+        <Text
+          fontFamily={theme.fontFamily.OverpassRegular}
+          fontSize={theme.fontSize.md20}
+          color={theme.colors.white}
+        >
+          Hoje
+        </Text>
+
+        <Styled.Next5DaysContainer
+          onPress={() =>
+            navigation.navigate("Next5Days", {
+              forecast: forecast,
+              forecast5Days: forecast5Days,
+            })
+          }
+        >
+          <Text
+            fontFamily={theme.fontFamily.OverpassRegular}
+            fontSize={theme.fontSize.xs16}
+            color={theme.colors.gray100}
+          >
+            Próximos 5 dias
+          </Text>
+
+          <SimpleLineIcons
+            name="arrow-right"
+            size={11}
+            color={theme.colors.gray100}
+            style={{ marginLeft: 4 }}
+          />
+        </Styled.Next5DaysContainer>
+      </Styled.TodayAnd5NextDaysContainer>
+
+      <Divider top={15} />
+
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={forecast.forecastday[0].hour}
+        keyExtractor={(_, index) => String(index)}
+        ItemSeparatorComponent={() => <Styled.Separator />}
+        renderItem={({ item, index }) => {
+          const dataCardHourTemperature = [
+            {
+              id: index,
+              icon: item.condition.icon,
+              temperatureValue: Math.round(item.temp_c),
+              hour: item.time.substring(11, 16),
+            },
+          ];
+          return (
+            <CardHourTemperature data={dataCardHourTemperature} key={index} />
+          );
+        }}
+      />
+
+      <Divider bottom={15} />
+    </>
+  );
+};
+
+const Home = ({ navigation }: Props): JSX.Element => {
+  const [city, setCity] = useState(null);
+  const [countryCode, setCountryCode] = useState(null);
+  const [response, setResponse] = useState<ISearchData>(null);
+  const [currentDate, setCurrentDate] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [forecast5Days, setForecast5Days] = useState<IForecast5Days>(null);
+
+  const getDate = () => {
+    setCurrentDate(formatDate());
+  };
+
+  const getCityName = useCallback(async () => {
+    setIsLoading(true);
+    const storedCity = await AsyncStorage.getItem(CITY_NAME);
+    const storedCountryCode = await AsyncStorage.getItem(COUNTRY_CODE);
+
+    setCountryCode(storedCountryCode);
+
+    setCity(storedCity);
+
+    setIsLoading(false);
+  }, []);
+
+  const getAPIData = async () => {
+    setIsLoading(true);
+
+    await FindWeatherAPI.getForecast(city)
+      .then((response) => {
+        const data = response.data;
+
+        setResponse(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error calling API: ", error);
+        setIsLoading(false);
+      });
+  };
+
+  const getForecast5Days = async () => {
+    setIsLoading(true);
+
+    await FindWeatherOpenWeatherAPI.getForecast(city, countryCode)
+      .then((res) => {
+        const data: IForecast5Days = res.data;
+
+        setForecast5Days(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error calling 5 next days forecast API: ", error);
+        setIsLoading(false);
+      });
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      setIsLoading(true);
+      getCityName();
+    }, [])
+  );
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 5000);
+
+  useEffect(() => {
+    if (city) {
+      getAPIData();
+      getDate();
+      getForecast5Days();
+    } else {
+      setIsLoading(false);
+      setResponse(null);
+      setForecast5Days(null);
+    }
+  }, [city]);
+
+  if (isLoading) {
+    return (
+      <Styled.ContainerLoading>
+        <ActivityIndicator size="large" color={theme.colors.white} />
+      </Styled.ContainerLoading>
+    );
+  } else if (!isLoading && !response) {
+    return <EmptyStateContent navigation={navigation} />;
+  }
+
+  return (
+    <>
+      {response && (
+        <SectionList
+          style={{ backgroundColor: theme.colors.dark, paddingHorizontal: 16 }}
+          sections={[
+            {
+              title: "",
+              data: [
+                <FullContent
+                  current={response.current}
+                  location={response.location}
+                  forecast={response.forecast}
+                  date={currentDate}
+                  forecast5Days={forecast5Days && forecast5Days.list}
+                  navigation={navigation}
+                />,
+              ],
+            },
+          ]}
+          renderItem={({ item }) => item}
+          keyExtractor={(_, index) => String(index)}
+        />
+      )}
+    </>
+  );
 };
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,13 +2,92 @@ import { Platform, StatusBar } from 'react-native';
 import styled from 'styled-components/native';
 import theme from '../../theme';
 
-const ScrollView = styled.ScrollView`
-	flex: 1;
-	background-color: ${theme.colors.dark};
-	padding: 0 16px;
+const getStatusBarHeight = Platform.OS === "android" ? StatusBar.currentHeight : 0;
+
+
+const Container = styled.SafeAreaView`
+  margin-top: ${getStatusBarHeight}px;
 `;
 
+const ContainerEmptyState = styled.View`
+  align-items: center;
+  background-color: ${theme.colors.dark};
+  justify-content: space-between;
+  height: 100%;
+`;
+
+const ScrollView = styled.ScrollView`
+  flex: 1;
+  background-color: ${theme.colors.dark};
+  padding: 0 16px;
+`;
+
+const LocationIconContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  align-items: flex-start
+`;
+
+const LocationTextContainer = styled.View``;
+
+const LocationCityCountryContainer = styled.View`
+  flex-direction: row;
+`;
+
+const ImageContainer = styled.View`
+  height: 170px;
+  align-self: center;
+`;
+
+const Image = styled.Image`
+  width: 170px;
+  height: 100%;
+`;
+
+const TodayAnd5NextDaysContainer = styled.View`
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const Next5DaysContainer = styled.TouchableOpacity.attrs(() => ({
+  activeOpacity: 0.6
+}))`
+  flex-direction: row;
+  align-items: center;
+  height: 30px;
+`;
+
+const Separator = styled.View`
+  margin-right: 10px;
+`;
+
+const ContainerTemperature = styled.View`
+  flex-direction: row;
+  align-self: center;
+`;
+
+const ContainerLoading = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.dark};
+`;
 
 export default {
-	ScrollView
+  Container,
+  ContainerEmptyState,
+  ScrollView,
+  LocationIconContainer,
+  LocationTextContainer,
+  LocationCityCountryContainer,
+  ImageContainer,
+  Image,
+  TodayAnd5NextDaysContainer,
+  Next5DaysContainer,
+  Separator,
+  ContainerTemperature,
+  ContainerLoading
 };
