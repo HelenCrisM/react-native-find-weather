22b07a080a9e4a051608432f9a88118d1a42b3f1
Helen Cris <helencristavares@gmail.com>
2024-03-26 21:01:21 -0300

    ✨ feat: add images

---
+++ src/storage/storage.config.ts
@@ -0,0 +1,7 @@
+const USER_FIRST_TIME = '@findweather:welcome';
+
+const CITY_NAME = '@findweather:city_name';
+
+const COUNTRY_CODE = '@findweather:country_code';
+
+export {USER_FIRST_TIME, CITY_NAME, COUNTRY_CODE}
